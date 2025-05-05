ceccad369478f4f51bb91eac70de656508f09d2b
yasindu2000 <yasindudahanayake@gmail.com>
2025-05-05 21:57:25 +0530

    add category component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
 
 function Home() {
   return (
     <div className='mt-10 '>
         <MainBanner/>
+        <Categories/>
     </div>
   )
 }
